bcf99ebd3f04c19b089533b85cb62e6a21537790
JohnCrabs <kavouras.ioannis@gmail.com>
2021-02-03 01:57:51 +0200

    Initial commit

---
+++ google_lib/my_gee.py
@@ -0,0 +1,78 @@
+import re
+import ee
+from my_lib import my_calendar_v2 as my_cal_v2
+import time
+
+ee.Initialize()
+
+
+def clear_tasks():
+    operation_list = ee.batch.data.listOperations()
+    # print(operation_list)
+    index = 1
+    list_len = len(operation_list)
+    for operation in operation_list:
+        print("(" + str(index) + "/" + str(list_len) + ") Cancel operation: " + operation['name'])
+        ee.batch.data.cancelOperation(operation['name'])
+        index += 1
+
+
+def download_image_from_collection(collection_id, image_band, img_name, list_date_range, list_countries, scale=5000,
+                                   waiting_time=None):
+    country_collection = ee.FeatureCollection('users/midekisa/Countries')  # add countries boundary geometries
+    for country in list_countries:
+        print("\nCreate boundary geometry for  " + country)
+        country_geometry = country_collection.filter(ee.Filter.eq('Country', country))
+        country_name_for_path = re.sub(r"[^\w\s]", '', country).replace(" ", "_")
+        # print(country_name_for_path)
+
+        for date_range in list_date_range:
+            start_date = date_range[0]
+            end_date = date_range[1]
+            print("\t* Prepare collection for period (" + start_date + " to " + end_date + "):")
+
+            collection = ee.ImageCollection(collection_id).filterDate(start_date, end_date).filterBounds(
+                country_geometry)
+            image = collection.mean().clip(country_geometry)
+            image = image.select([image_band])
+
+            start_date_for_path = my_cal_v2.change_date_format_from_string(str_date=start_date,
+                                                                           date_format_from=my_cal_v2.YYYY_MM_DD,
+                                                                           date_format_to=my_cal_v2.YYYY_MM_DD,
+                                                                           date_delimeter_from=my_cal_v2.del_dash,
+                                                                           date_delimeter_to=my_cal_v2.del_none,
+                                                                           century=21)
+            end_date_for_path = my_cal_v2.change_date_format_from_string(str_date=end_date,
+                                                                         date_format_from=my_cal_v2.YYYY_MM_DD,
+                                                                         date_format_to=my_cal_v2.YYYY_MM_DD,
+                                                                         date_delimeter_from=my_cal_v2.del_dash,
+                                                                         date_delimeter_to=my_cal_v2.del_none,
+                                                                         century=21)
+
+            my_description = img_name + '_' + country_name_for_path + '_' + str(scale) + '_' + start_date_for_path + '_' + end_date_for_path
+            print("\t\tExport Image via description (task/image name):")
+            print("\t\t" + my_description)
+
+            task_bash = {
+                'image': image,
+                'description': my_description,
+                'region': country_geometry.geometry(),
+                'scale': scale,
+                'folder': 'GEE_' + country_name_for_path
+            }
+            task = ee.batch.Export.image.toDrive(**task_bash)
+            task.start()
+            print("Run task on https://code.earthengine.google.com/ task window.")
+            is_timer = False
+            timer_value = 0
+            if waiting_time is not None:
+                is_timer = True
+                timer_value = waiting_time
+            while task.status()['state'] is 'READY':
+                if is_timer:
+                    time.sleep(secs=1)
+                    timer_value -= 1
+                    if timer_value == 0:
+                        break
+
+            print("Task finished successfully!")
---
+++ main.py
@@ -0,0 +1,136 @@
+import pandas as pd
+from my_lib import my_calendar_v2 as my_cal_v2
+from google_lib import my_gee
+
+# ----------------------------------------- #
+# ---------- 1) Find Date Ranges ---------- #
+# ----------------------------------------- #
+
+start_date = "2020-01-01"  # "YYYY-MM-DD"
+end_date = "2020-12-31"  # "YYYY-MM-DD"
+
+# Create period ster a multiple of week period (by week I dont mean Monday to Sunday necessary, but a 7 day in a row)
+period_alpha = 1
+period_step = period_alpha*7
+
+list_date_period_ranges = my_cal_v2.break_date_range_to_periods(date_start=start_date, date_end=end_date,
+                                                                period_step=period_step,
+                                                                date_format=my_cal_v2.YYYY_MM_DD,
+                                                                date_delimeter=my_cal_v2.del_dash, century=21)
+# print(list_date_period_ranges)
+
+
+# ------------------------------------------------- #
+# ---------- 3) Create Datasets From CSV ---------- #
+# ------------------------------------------------- #
+
+
+'''
+# ############################
+# ### DATASET COLUMN NAMES ###
+# ############################
+# COUNTRY: Covid, Pollution               ID: Covid, Pollution                       DATE: Covid Pollution
+# RETAIL_AND_RECREATIONAL: None           GROCERY_AND_PHARMACY: None                 PARKS,TRANSIT_STATION: None
+# WORKPLACES,RESIDENTIAL: None            TESTING_POLICY: None                       CONTACT_TRACING: Covid
+# CONTAINMENT_INDEX: None                 STRINGENCY_INDEX: None                     VACCINATION_POLICY: Covid
+# DEPT_RELIEF: None                       FACIAL_COVERING: None                      INCOME_SUPPORT: None
+# RESTRICTIONS_INTERNAL_MOVEMENTS: Covid, Pollution   
+# INTERNATIONAL_TRAVEL_CONTROLS: Covid, Pollution
+# CONTINENT: Covid, Pollution             TOTAL_CASES: Covid                         NEW_CASES: Covid
+# NEW_CASES_SMOOTHED: Covid               TOTAL_DEATHS: Covid                        NEW_DEATHS: Covid
+# NEW_DEATHS_SMOOTHED: Covid              TOTAL_CASES_PER_MILLION: Covid             NEW_CASES_PER_MILLION: Covid
+# NEW_CASES_SMOOTHED_PER_MILLION: Covid   TOTAL_DEATHS_PER_MILLION: Covid            NEW_DEATHS_PER_MILLION: Covid
+# NEW_DEATHS_SMOOTHED_PER_MILLION: Covid  REPRODUCTION_RATE: Covid                   ICU_PATIENTS: Covid
+# ICU_PATIENTS_PER_MILLION: Covid         HOSP_PATIENTS: Covid                       HOSP_PATIENTS_PER_MILLION: Covid
+# WEEKLY_ICU_ADMISSIONS: Covid            WEEKLY_ICU_ADMISSIONS_PER_MILLION: Covid   WEEKLY_HOSP_ADMISSIONS: Covid
+# WEEKLY_HOSP_ADMISSIONS_PER_MILLION: Covid
+# NEW_TEST: Covid                         TOTAL_TESTS: Covid                         TOTAL_TESTS_PER_THOUSAND: Covid
+# NEW_TESTS_PER_THOUSAND: Covid           NEW_TESTS_SMOOTHED: Covid
+# NEW_TESTS_SMOOTHED_PER_THOUSAND: Covid  POSITIVE_RATE: Covid                       TESTS_PER_CASE: Covid
+# TESTS_UNITS: Covid                      TOTAL_VACCINATIONS: Covid                  NEW_VACCINATIONS: Covid
+# TOTAL_VACCINATIONS_PER_HUNDRED: Covid   NEW_VACCINATIONS_PER_MILLION: Covid        POPULATION: Covid, Pollution
+# POPULATION_DENSITY: Covid, Pollution    MEDIAN_AGE: Covid                          AGED_65_OLDER: Covid
+# AGED_70_OLDER: Covid                    GDP_PER_CAPITA: Covid                      EXTREME_POVERTY: Covid
+# CARDIOVASC_DEATH_RATE: Covid            DIABETES_PREVALENCE: Covid                 FEMALE_SMOKERS: Covid
+# MALE_SMOKERS: Covid                     HANDWASHING_FACILITIES: Covid              HOSPITAL_BEDS_PER_THOUSAND: Covid
+# LIFE_EXPECTANCY: Covid                  HUMAN_DEVELOPMENT_INDEX: None              PUBLIC_INFORMATION_CAMPAIGNS: Covid
+# CANCEL_PUBLIC_EVENTS: Covid, Pollution
+# RESTRICTION_GATHERINGS: Covid, Pollution
+# CLOSE_PUBLIC_TRANSPORT: Covid, Pollution
+# SCHOOL_CLOSURES: Covid, Pollution
+# STAY_HOME_REQUIREMENTS: Covid, Pollution
+# WORKPLACE_CLOSURES: Covid, Pollution
+'''
+
+
+def find_unique_values_list(list_input: []):
+    list_output = []
+    for item in list_input:
+        if item not in list_output:
+            list_output.append(item)
+    return list_output
+
+
+str_covid_measures_csv_path = "Data/covid_measures.csv"
+df_covid_measures = pd.read_csv(str_covid_measures_csv_path, low_memory=False)
+# print(df_covid_measures.head())
+
+list_unique_countries = df_covid_measures.COUNTRY.unique()
+# print(list_unique_countries)
+# print(len(list_unique_countries))
+
+df_pollution_dataset = df_covid_measures[['COUNTRY', 'ID', 'DATE', 'RESTRICTIONS_INTERNAL_MOVEMENTS',
+                                          'INTERNATIONAL_TRAVEL_CONTROLS', 'CONTINENT', 'POPULATION',
+                                          'POPULATION_DENSITY', 'CANCEL_PUBLIC_EVENTS', 'RESTRICTION_GATHERINGS',
+                                          'CLOSE_PUBLIC_TRANSPORT', 'SCHOOL_CLOSURES', 'STAY_HOME_REQUIREMENTS',
+                                          'WORKPLACE_CLOSURES']]
+df_covid_dataset = df_covid_measures[['COUNTRY', 'ID', 'CONTACT_TRACING', 'VACCINATION_POLICY',
+                                      'RESTRICTIONS_INTERNAL_MOVEMENTS', 'INTERNATIONAL_TRAVEL_CONTROLS',
+                                      'CONTINENT', 'TOTAL_CASES', 'NEW_CASES_SMOOTHED', 'TOTAL_DEATHS',
+                                      'NEW_CASES', 'NEW_CASES_SMOOTHED', 'TOTAL_CASES_PER_MILLION',
+                                      'NEW_CASES_PER_MILLION', 'NEW_CASES_SMOOTHED_PER_MILLION', 'REPRODUCTION_RATE',
+                                      'ICU_PATIENTS', 'ICU_PATIENTS_PER_MILLION', 'HOSP_PATIENTS',
+                                      'HOSP_PATIENTS_PER_MILLION', 'WEEKLY_ICU_ADMISSIONS',
+                                      'WEEKLY_ICU_ADMISSIONS_PER_MILLION', 'WEEKLY_HOSP_ADMISSIONS',
+                                      'WEEKLY_HOSP_ADMISSIONS_PER_MILLION', 'NEW_TESTS', 'TOTAL_TESTS',
+                                      'TOTAL_TESTS_PER_THOUSAND', 'NEW_TESTS_PER_THOUSAND', 'NEW_TESTS_SMOOTHED',
+                                      'NEW_TESTS_SMOOTHED_PER_THOUSAND',  'POSITIVE_RATE', 'TESTS_PER_CASE',
+                                      'TESTS_UNITS', 'TOTAL_VACCINATIONS', 'NEW_VACCINATIONS',
+                                      'TOTAL_VACCINATIONS_PER_HUNDRED', 'NEW_VACCINATIONS_PER_MILLION', 'POPULATION',
+                                      'POPULATION_DENSITY', 'MEDIAN_AGE', 'AGED_65_OLDER', 'AGED_70_OLDER',
+                                      'GDP_PER_CAPITA', 'EXTREME_POVERTY', 'CARDIOVASC_DEATH_RATE',
+                                      'DIABETES_PREVALENCE', 'FEMALE_SMOKERS', 'MALE_SMOKERS',
+                                      'HANDWASHING_FACILITIES', 'HOSPITAL_BEDS_PER_THOUSAND', 'LIFE_EXPECTANCY',
+                                      'HUMAN_DEVELOPMENT_INDEX', 'PUBLIC_INFORMATION_CAMPAIGNS', 'CANCEL_PUBLIC_EVENTS',
+                                      'RESTRICTION_GATHERINGS', 'CLOSE_PUBLIC_TRANSPORT', 'SCHOOL_CLOSURES',
+                                      'STAY_HOME_REQUIREMENTS', 'WORKPLACE_CLOSURES']]
+
+
+# --------------------------------------------------- #
+# ---------- 3) Download Sentiner-5 Images ---------- #
+# --------------------------------------------------- #
+
+# my_gee.clear_tasks()
+list_collection_id = ['COPERNICUS/S5P/OFFL/L3_O3', 'COPERNICUS/S5P/OFFL/L3_CO', 'COPERNICUS/S5P/OFFL/L3_AER_AI',
+                      'COPERNICUS/S5P/OFFL/L3_HCHO', 'COPERNICUS/S5P/OFFL/L3_SO2',
+                      'COPERNICUS/S5P/OFFL/L3_NO2']
+list_colection_bands = ['O3_column_number_density', 'CO_column_number_density', 'absorbing_aerosol_index',
+                        'tropospheric_HCHO_column_number_density', 'SO2_column_number_density',
+                        'NO2_column_number_density']
+list_collection_names = ['ozone_O3_density', 'carbon_monoxide_CO_density', 'absorbing_aerosol_index',
+                         'offline_formaldehyde_HCHO_density', 'sulphur_dioxide_SO2_density'
+                         'nitrogen_dioxide_NO2_density']
+
+
+list_S5_data_len = len(list_collection_id)
+scale_m_per_px = 5000
+waiting_minute_multiplier = 5
+waiting_time_in_sec = int(waiting_minute_multiplier)*60
+for i in range(0, list_S5_data_len):
+    my_gee.download_image_from_collection(collection_id=list_collection_id[i],
+                                          image_band=list_colection_bands[i],
+                                          img_name=list_collection_names[i],
+                                          list_date_range=list_date_period_ranges,
+                                          list_countries=list_unique_countries,
+                                          scale=scale_m_per_px,
+                                          waiting_time=waiting_time_in_sec)
